046d6202094f9c830758f110205dce6b34b5cde5
VEERAKUMAR T <veerakumarthangavel4@gmail.com>
2024-03-06 14:25:02 +0530

    bid update

--- main.cjs
+++ main.cjs
@@ -89,7 +89,7 @@ db.collection('R15Bidding').insertOne(request.body).then(function(){
 })
 })
 //to sort out last element
-app.get('bikebid/1/bidresult',function(request,response){
+app.get('/bikebid/1/bidresult',function(request,response){
     const lastelement = []
     db.collection('R15Bidding').find().sort({ _id: -1 }).limit(1)
     .forEach(element=>  lastelement.push(element))
